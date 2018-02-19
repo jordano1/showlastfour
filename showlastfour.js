169da74e4adc2f8494aa432eda29126d3987d92b
jordan olson <jordanolson1@gmail.com>
2018-02-19 15:18:09 -0700

    added a line that allows string/period/comma/slash replacements

--- showlastfour.js
+++ showlastfour.js
@@ -1,11 +1,16 @@
 
 // return masked string
 function maskify(cc) {
-	var dd = cc.toString();
-	//dd will be used, it is sanitized toString() and is easier to work with with regex
-	var mask = dd.replace(/.(?=.{4,}$)/g, '#');
-	//I used above regex statement from the user MinusFour this url: https://stackoverflow.com/questions/35256611/replace-all-chars-with-except-for-last-4/35256715
-	return mask;
+	// return masked string
+	var pizza = "asdf"
+	var rizza = 123124214
+	var trizza = "asdfoijasofijawofijewofijw19024419085775klsdjfl.,xcz.v,mn./.z,mx./v/x,.mv.,zx"
+	function maskify(cc) {
+		var dd = cc.toString();
+		var mask = dd.replace(/\d(?=.{4,}$)|\w(?=.{4,}$)|[.,/](?=.{4,}$)/g, "#");
+		console.log(mask);
+		return mask;
+	}
 }
 
 
@@ -36,10 +41,16 @@ function maskify(cc) {
 	return maskedString;
 }
 */
-
+console.log(maskify(124124124))
+console.log(maskify(pizza))
+console.log(maskify(rizza))
+console.log(maskify(trizza))
 console.log(maskify('4556364607935616')); // '############5616');
 console.log(maskify('1')); // '1');
+console.log(maskify('12')); // '1');
+console.log(maskify('123')); // '1');
+console.log(maskify('1234')); // '1');
 console.log(maskify('11111')); // '#1111');
-console.log(maskify('22222222222222')); // '#1111');
+console.log(maskify('22222222222222')); // '#########2222');
 console.log(maskify(1)); // '#1111');
-console.log(maskify(4444444444)); // '#1111');
+console.log(maskify(4444444444)); // '#####4444');
